f32e04687eb989cf51ab9bfec22506274379fc48
mirbek10 <mirbekbook@gmail.com>
2025-12-29 21:51:11 +0600

    sidebar

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { RouterProvider } from "react-router";
 import router from "./router";
 import Banner from "../shared/ui/banner/Banner";
+import UserProfileSidebar from "../shared/ui/sidebar/UserProfileSidebar";
 
 function App() {
   return (
     <div>
       {/* <RouterProvider router={router} /> */}
-      <Banner/>
+      {/* <Banner/> */}
+      <UserProfileSidebar/>
     </div>
   );
 }
